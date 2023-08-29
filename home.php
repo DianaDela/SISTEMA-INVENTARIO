e90ca1e7b54e08526d53406fdc106844b44c75f6
Diana de la O <delaoitzel17@gmail.com>
2023-08-29 11:11:16 -0600

    Nuevos cambios en el sistema

--- home.php
+++ home.php
@@ -4,12 +4,89 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="assent/navbar.css">
 </head>
 <body>
-    <h1>
-        ¡Bienvenido!
-    </h1>
+    <div class="container">
+    <div class="sidebar close">
+    <div class="logo-details">
+    <i class='bx bx-home' ></i>
+      <span class="logo_name">Grupo Piasa</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class='bx bx-grid-alt' ></i>
+          <span class="link_name">Dashboard</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Dashboard</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+          <i class='bx bx-user'></i>
+            <span class="link_name">Usuarios</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow' ></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Usuarios</a></li>
+          <li><a href="">Nuevo usuario</a></li>
+          <li><a href="">Usuarios</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="">
+        <i class='bx bxs-file-doc' style='color: white;'></i>
+          <span class="link_name">Historial</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Historial</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="">
+        <i class='bx bx-edit-alt'></i>
+          <span class="link_name">Movimientos</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="">Movimientos</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="index.php">
+        <i class='bx bx-log-out' ></i>
+          <span class="link_name">Cerrar Sesión</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="index.php">Cerrar Sesión</a></li>
+        </ul>
+      </li>
+      <li>
+    <div class="profile-details">
+      <div class="profile-content">
+        <!--<img src="image/profile.jpg" alt="profileImg">-->
+      </div>
+      <div class="name-job">
+        <div class="profile_name">Administrador</div>
+        <div class="job">T.I</div>
+      </div>
+      <i class='bx bx-user'></i>
+    </div>
+  </li>
+</ul>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bx-menu' ></i>
+      <span class="text">Dashboard</span>
+    </div>
+  </section>
+    </div>
+
+    <script src="js/navbar.js"></script>
 </body>
 </html>
